3e9647ba0b18978f1046f0eefed30523de938126
htagourti <htagourti@linagora.com>
2024-12-12 10:20:12 +0000

    small changes to  nginx & cerbot script

--- enable_letsencrypt.sh
+++ enable_letsencrypt.sh
@@ -2,7 +2,7 @@
 
 # Description: Simplified script to obtain and install Let's Encrypt SSL certificates using Certbot.
 
-DOMAIN="dne.linagora.com"
+DOMAIN="demo-lucie.linagora.com"
 EMAIL="htagourti@linagora.com"
 NGINX_CONF_PATH="/home/ubuntu/projects/webui/conf.d"  # Path to your Nginx configuration folder
 
@@ -18,8 +18,8 @@ fi
 sudo certbot certonly --standalone -d "$DOMAIN" --non-interactive --agree-tos -m "$EMAIL"
 
 # Copy SSL certificate to the project folder to mount to nginx docker
-sudo cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem /home/ubuntu/projects/webui/certbot/
-sudo cp /etc/letsencrypt/live/$DOMAIN/privkey.pem /home/ubuntu/projects/webui/certbot/
+sudo cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem ./certbot/
+sudo cp /etc/letsencrypt/live/$DOMAIN/privkey.pem ./certbot/
 
 
 echo "Let's Encrypt SSL certificate has been installed"
